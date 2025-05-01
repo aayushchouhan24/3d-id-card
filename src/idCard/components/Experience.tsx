df044be1081cdb9b4efbe94f1fc81a47daa60462
Aayush Chouhan <aayushchouhan24@gmail.com>
2025-05-02 04:10:36 +0530

    refactor(idCard): simplify card rotation logic and adjust light intensity
    Removed redundant `isFlipped` state and streamlined the rotation logic in the Card component. Adjusted the intensity of Lightformer components in the Experience component for better visual effects. Improved code readability by reducing unnecessary line breaks and consolidating imports.

--- src/idCard/components/Experience.tsx
+++ src/idCard/components/Experience.tsx
@@ -15,7 +15,8 @@ const Experience = (student: User) => {
         <Lightformer intensity={10} color="white" position={[0, -1, 5]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
         <Lightformer intensity={3} color="white" position={[-1, -1, 1]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
         <Lightformer intensity={2} color="white" position={[1, 1, 1]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
-        <Lightformer toneMapped intensity={3} color="white" position={[-10, 0, 14]} rotation={[0, Math.PI / 2, Math.PI / 3]} scale={[500, 10, 1]} />
+        <Lightformer toneMapped intensity={5} color="white" position={[-10, 0, 14]} rotation={[0, Math.PI / 2, Math.PI / 3]} scale={[500, 10, 1]} />
+        <Lightformer toneMapped intensity={3} color="white" position={[15, 0, 10]} scale={[10, 10, 1]} />
       </Environment>
     </Canvas>
   )
